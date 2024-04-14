36d998aa0159517c089dc07140537c32cb76ef2a
siwar said <siwarsaid8@gmail.com>
2024-04-14 13:28:39 +0200

    quete classes

---
+++ lesClasses/Character.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesClasses
+{
+    internal class Character
+    {
+        public string Nom{  get; set; }
+        public int PointDeVie { get; set; }
+        public int ForceAttaque {  get; set; }
+        public int ForceDefense {  get; set; }
+       
+        
+        public Character(string nom,int pv,int foratta,int forDef)
+        { 
+            Nom = nom;
+            PointDeVie = pv;
+            ForceAttaque = foratta;
+            ForceDefense = forDef;
+        }
+        public bool IsAlive()
+        {
+            bool vivant = true;
+            bool mort = false;
+
+            if (PointDeVie > 0)
+            {
+                return vivant;
+            }
+            else
+            {
+                return mort;
+            }
+
+        }
+        public void Attack(Character attaque)
+        {
+            if (PointDeVie > 0)
+            {
+                attaque.PointDeVie = attaque.PointDeVie - Math.Abs(ForceAttaque - attaque.ForceDefense);
+            } 
+        }
+    }
+}
---
+++ lesClasses/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace lesClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Character joueur1 = new Character("toto", 100, 50, 70);
+            Character joueur2 = new Character("titi", 200, 300, 60);
+
+            while (joueur1.IsAlive() && joueur2.IsAlive())
+            {
+                joueur1.Attack(joueur2);
+
+                if (joueur2.IsAlive())
+                {
+                    joueur2.Attack(joueur1);
+                }
+            }
+            if(joueur1.IsAlive())
+            {
+                Console.WriteLine($" {joueur1.Nom} a gagné ");
+            }
+            else
+            {
+                Console.WriteLine($" {joueur2.Nom} a gagne ");
+            }
+        }
+    }
+
+           
+
+}
+
